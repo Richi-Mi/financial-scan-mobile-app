005edc01ecc237f45854e05baa668bcce9019032
Mendoza Castañeda José Ricardo <mendozacastanedajosericardo@gmail.com>
2026-04-07 15:11:12 -0600

    ROOM database model implemented

---
+++ app/src/main/java/com/richi_mc/myapplication/data/model/TicketEntity.kt
@@ -0,0 +1,27 @@
+package com.richi_mc.myapplication.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "tickets")
+data class TicketEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id : Long = 0,
+
+    @ColumnInfo(name = "date")
+    val date : Date,
+
+    @ColumnInfo(name = "trade")
+    val trade : String, // O comercio
+
+    @ColumnInfo(name = "product_name")
+    val productName : String,
+
+    @ColumnInfo(name = "price")
+    val price : Float,
+
+    @ColumnInfo(name = "category")
+    val category : String
+)
