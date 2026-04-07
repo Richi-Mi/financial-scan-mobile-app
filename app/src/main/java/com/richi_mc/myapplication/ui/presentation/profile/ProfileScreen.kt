432637aeb77416d9f904acec60db7259dde55527
Luca Barcenas Pineda <luca.barcenas.pineda@gmail.com>
2026-04-07 17:30:15 -0600

    Profile Screen finished

---
+++ app/src/main/java/com/richi_mc/myapplication/ui/presentation/profile/ProfileScreen.kt
@@ -0,0 +1,9 @@
+package com.richi_mc.myapplication.ui.presentation.profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen() {
+    Text("Profile Screen")
+}
